661b2e76073a7510b1127f4be733e422942b8028
Lucas <lucaspiegas25@gmail.com>
2026-08-30 15:13:39 -0300

    La pila de salida sale de atras del farol, y la ficha vuelve a ser un cuadrado

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788111951|2558155';
+const CACHE_VERSION = '1788113582|2432317';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

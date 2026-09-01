e18e88402a38cd34c4f0ad11a18e2803de75ce42
Lucas <lucaspiegas25@gmail.com>
2026-09-01 15:51:50 -0300

    El cuarto cuadro de la intro dice como se llama esto

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788286790|2068793';
+const CACHE_VERSION = '1788288673|1922755';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

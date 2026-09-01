aba753fd02329e82dbc9902101623f2d2105d766
Lucas <lucaspiegas25@gmail.com>
2026-09-01 11:49:42 -0300

    Los contadores se encienden despues de la vitrina

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788273164|2184713';
+const CACHE_VERSION = '1788274145|1924428';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

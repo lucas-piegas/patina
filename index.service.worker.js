8e8896141ecb975ff4f67a6e7e490374182cc6e9
Lucas <lucaspiegas25@gmail.com>
2026-09-01 13:50:58 -0300

    El arranque no repite la primera pieza

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788279037|2551230';
+const CACHE_VERSION = '1788281418|1981109';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

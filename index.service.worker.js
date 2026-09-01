d508cd13e49c88ebd9038feb4f19b631e0c22f66
Lucas <lucaspiegas25@gmail.com>
2026-09-01 12:59:15 -0300

    El oro es el color de lo que se toca

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788275898|2012885';
+const CACHE_VERSION = '1788278318|2164896';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

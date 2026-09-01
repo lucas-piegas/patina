b3230dc231d17d52d0b52101e3e024dc385db421
Lucas <lucaspiegas25@gmail.com>
2026-09-01 12:18:54 -0300

    El cartel ya no tapa la pieza y los contadores son chapas

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788274145|1924428';
+const CACHE_VERSION = '1788275898|2012885';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

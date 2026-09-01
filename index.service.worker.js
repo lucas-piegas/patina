24f844952d260ef64642d8042a71435ad0f73451
Lucas <lucaspiegas25@gmail.com>
2026-09-01 15:20:36 -0300

    Detras del banco hay un taller en penumbra

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788282336|2038244';
+const CACHE_VERSION = '1788286790|2068793';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

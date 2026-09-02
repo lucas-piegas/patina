98631082a9f43f19043cbd2dc948247c9ac22337
Lucas <lucaspiegas25@gmail.com>
2026-09-02 08:59:52 -0300

    El vendedor de la mudanza
    Build del 2 de septiembre de 2026. Sobre el anterior:
    
    - El vendedor ambulante tiene su propio pop-up, con el molde del visitante: la
      silueta a contraluz, su frase, lo que ofrece y las respuestas.
    - En el panel de la mudanza queda una puerta que lo abre.
    - La pieza que pide cuando no te alcanza la plata se nombra con "tu" y no con
      "la": el catalogo no guarda el genero de cada pieza.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788315892|2104206';
+const CACHE_VERSION = '1788350343|2046371';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

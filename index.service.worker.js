f6952cf82c5d723657fc1ac3c52d681209dc08b9
Lucas <lucaspiegas25@gmail.com>
2026-09-02 09:55:30 -0300

    El panel de la mudanza, mas grande y mas claro
    El contenido de los bloques crece con el bloque, limitado por el ancho y no
    solo por el alto. Y "la linea" pasa a llamarse "las estaciones", que es la
    palabra paralela a la del otro bloque.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788352124|2385354';
+const CACHE_VERSION = '1788353692|1988003';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

beda0a7e23bf32f2c54581abf30050bd1993bf2f
Lucas <lucaspiegas25@gmail.com>
2026-09-08 09:13:26 -0300

    Prueba de ritmo quieto en el telefono: 15 fps quieto, dormir a los 30 s, y los ocho detalles

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788833119|2115953';
+const CACHE_VERSION = '1788869561|2071681';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

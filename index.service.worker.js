e0267e74bae1ae72f4aa24d5a256c9d0ed7dfdab
Lucas <lucaspiegas25@gmail.com>
2026-09-08 13:43:30 -0300

    Sin el sueno de los 30 segundos: el taller ya no se congela
    La prueba anterior lo contesto: quedar sin actividad medio minuto se veia
    como un tildado, y en la web no ahorraba nada igual (quieta 124% de un
    nucleo, dormida 108%). Vuelve a dibujar siempre.
    
    En su lugar el juego suelta el candado de pantalla encendida, asi el telefono
    la apaga con su tiempo de espera de siempre. Quedan los 15 cuadros con la
    pantalla quieta y la fisica apagada, que no se ven.
    
    Primera carga en gzip 9,27 MB, igual que antes: solo cambia el .pck.

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788870386|2255109';
+const CACHE_VERSION = '1788885758|2337611';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

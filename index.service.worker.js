416d3db1d6a76809ed3a1818657c0624489739f3
Lucas <lucaspiegas25@gmail.com>
2026-09-16 20:33:04 -0300

    Abrir la lista de efectos es leerlos

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789599379|2274943';
+const CACHE_VERSION = '1789601548|2069147';
 /** @type {string} */
 const CACHE_PREFIX = 'Patina-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
